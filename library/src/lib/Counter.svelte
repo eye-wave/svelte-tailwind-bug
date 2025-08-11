11c7b04babee6d6357d58e30feea2c14b81eb054
_eyewave <89079979+eye-wave@users.noreply.github.com>
2025-08-11 17:52:07 +0200

    upload code

---
+++ library/src/lib/Counter.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  
+  type Props = {
+    value?: number
+  }
+  
+  let { value = $bindable(0) }: Props = $props()
+  
+</script>
+
+<button class="bg-red-600 text-blue-600" onclick={() => value += 1}>{value}</button>
